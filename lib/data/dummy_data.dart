c93ddb70fa54e7c5359bc7cef6360bdced0b19e7
gyhaan <g.yhaan@alustudent.com>
2025-02-07 14:08:30 +0200

    created dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:pixels/models/product.dart';
+
+const pixelList = [
+  Product(
+      title: "Pixels",
+      subtitle: "pixel 1",
+      color: Colors.blue,
+      description: "Pixels is the most featureful phone ever",
+      price: 800,
+      rating: 0),
+  Product(
+      title: "Laptop",
+      subtitle: "laptop",
+      color: Colors.green,
+      description: "Laptop is the most productive development tool",
+      price: 800,
+      rating: 0),
+  Product(
+      title: "Tablet",
+      subtitle: "tablet",
+      color: Colors.amber,
+      description: "Tablet is the most useful device ever for meeting",
+      price: 800,
+      rating: 3),
+  Product(
+      title: "PenDrive",
+      subtitle: "pen drive",
+      color: Colors.brown,
+      description: "iPhone is the stylish phone ever",
+      price: 800,
+      rating: 0),
+  Product(
+      title: "FloppyDrive",
+      subtitle: "floppy",
+      color: Colors.lightBlueAccent,
+      description: "iPhone is the stylish phone ever",
+      price: 800,
+      rating: 0),
+];
